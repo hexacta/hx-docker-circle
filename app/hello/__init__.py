f593cad63654532cad5906f6937c728dc38c27cb
Fausto Carrera <fausto.carrera.f@gmail.com>
2017-06-10 14:31:15 -0300

    add json response

--- app/hello/__init__.py
+++ app/hello/__init__.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -7,9 +8,14 @@ app = Flask(__name__)
 def hello():
     return render_template('hello.html')
 
-@app.route('/test')
-def test():
-    return 'this is a test'
+@app.route('/v1/user/<int:user_id>')
+def user(user_id):
+    return jsonify({
+        'id': user_id,
+        'name': 'Jhon Doe',
+        'email': 'jhon.doe@nowhere.com',
+        'last_login': '2017-01-01 12:00:00'
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
